ba92960b601364c183842a8a043ee5d2c6bccb71
ram <achmedovramil@gmail.com>
2024-06-25 19:13:33 +0300

    test:lesson-24

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,11 @@
-  
+const mobileContainer = document.querySelector(".mobile-container");
+const burger = document.querySelector(".hamburger");
+const body = document.querySelector("body");
+
+function addClass() {
+	mobileContainer.classList.toggle("show");
+	burger.classList.toggle("is-active");
+	body.classList.toggle("overflow-hidden");
+}
+
+burger.addEventListener("click", addClass);
