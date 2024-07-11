b3e73a4660208df52c2f0c30173b55b0a1ae3e1c
ram <achmedovramil@gmail.com>
2024-07-11 15:15:15 +0300

    text:lesson-28

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -10,7 +10,7 @@ function addClass() {
 
 burger.addEventListener("click", addClass);
 
-const swiper = new Swiper(".banner-slider", {
+const swiperBanner = new Swiper(".banner-slider", {
 	loop: true,
 	spaceBetween: 20,
 
@@ -47,3 +47,36 @@ downloadFile.forEach(item => {
 		fileName.innerHTML = item.files[0].name;
 	})
 });
+// Swiper advantages
+const advantagesSlideLength = document.querySelectorAll('.advantages .swiper-slide').length;
+console.log(advantagesSlideLength);
+
+const swiperAdvantages = new Swiper('#advantages', {
+  	speed: 400,
+	spaceBetween: 12,
+	loop: true,
+	simulateTouch: true,
+	slidesPerView: 1,
+
+	pagination: {
+		el: '.swiper-pagination',
+		enabled: true,
+		clickable: true,
+	},
+
+	breakpoints: {
+		1025: {
+			spaceBetween: 0,
+			loop: false,
+			slidesPerView: 4,
+			slidesPerGroup: advantagesSlideLength,
+			simulateTouch: false,
+
+			pagination: {
+				enabled: false,
+			}
+		}
+	}
+});
+
+
